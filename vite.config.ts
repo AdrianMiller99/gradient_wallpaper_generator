ad56e9ba455a6e6d3d909aec54f67f339e47abb9
Adrian Miller <adrian.miller@students.fhnw.ch>
2024-12-21 02:38:37 +0100

    this must be the final commit before it deploys

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/gradient_wallpaper_generator/',
+  base: '/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
